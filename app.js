a13fffe2cacc85c4cf70da2fce59084536627771
ankitkr47 <ankitkumardavmuz@gmail.com>
2023-12-13 11:58:57 +0530

    blogify

---
+++ app.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const express = require('express');
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log("MongoDB Connected"));
+
+
+const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/blog", blogRoute);
+
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
+})
